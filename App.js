aa65f9950562ab564edc8d7e0c891d7ea97435b0
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-06-15 17:22:24 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,103 +1,106 @@
 import React, { useState } from 'react';
-import './FoodApp.css';
-import burgerImage from './images/burger.jpg';
-import pizzaImage from './images/pizza.jpg';
-import saladImage from './images/salad.jpg';
+import './App.css';
 
-const FoodApp = () => {
+const App = () => {
   const [cartItems, setCartItems] = useState([]);
+  const [showCart, setShowCart] = useState(false);
 
-  const addToCart = (item) => {
-    setCartItems([...cartItems, { name: item, quantity: 1, price: getItemPrice(item) }]);
-  };
+  const tshirts = [
+    {
+      id: '1',
+      name: 'Navy Blue Armani T-shirt',
+      description: 'Premium quality navy blue T-shirt by Armani',
+      price: 29.99,
+      quantity: 10,
+    },
+    // Add more T-shirt objects here
+  ];
 
-  const removeFromCart = (item) => {
-    const updatedCart = cartItems.filter((cartItem) => cartItem.name !== item);
-    setCartItems(updatedCart);
-  };
-
-  const incrementQuantity = (item) => {
-    const updatedCart = cartItems.map((cartItem) => {
-      if (cartItem.name === item) {
-        return { ...cartItem, quantity: cartItem.quantity + 1 };
-      }
-      return cartItem;
-    });
-    setCartItems(updatedCart);
-  };
-
-  const decrementQuantity = (item) => {
-    const updatedCart = cartItems.map((cartItem) => {
-      if (cartItem.name === item && cartItem.quantity > 1) {
-        return { ...cartItem, quantity: cartItem.quantity - 1 };
-      }
-      return cartItem;
-    });
-    setCartItems(updatedCart);
-  };
-
-  const getItemPrice = (item) => {
-    // Return the price of the item based on its name
-    if (item === 'Burger') {
-      return 10;
-    } else if (item === 'Pizza') {
-      return 12;
-    } else if (item === 'Salad') {
-      return 8;
+  const addToCartHandler = (tshirt) => {
+    if (tshirt.quantity > 0) {
+      setCartItems((prevCartItems) => [...prevCartItems, tshirt]);
+      tshirt.quantity -= 1;
     }
-    return 0;
   };
 
-  const calculateTotalPrice = () => {
-    return cartItems.reduce((total, item) => total + item.quantity * item.price, 0);
+  const showCartHandler = () => {
+    setShowCart(true);
   };
+
+  const hideCartHandler = () => {
+    setShowCart(false);
+  };
+
+  const cartContent = (
+    <div className="cart">
+      <h2>Your Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Cart is empty</p>
+      ) : (
+        <>
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id}>
+                {item.name} - ${item.price.toFixed(2)}
+              </li>
+            ))}
+          </ul>
+          <p>Total: ${cartItems.reduce((total, item) => total + item.price, 0).toFixed(2)}</p>
+        </>
+      )}
+      <button className="order-button">Order</button>
+      <button className="close-button" onClick={hideCartHandler}>
+        Close
+      </button>
+    </div>
+  );
 
   return (
-    <div className="container">
-      <h1>Food App</h1>
-      <h2>Delicious Food Delivered To You</h2>
-      <div className="menu">
-        <div className="food-item">
-          <img src={burgerImage} alt="Burger" />
-          <p>Burger</p>
-          <p>Price: $10</p>
-          <button onClick={() => addToCart('Burger')}>Add to Cart</button>
+    <div>
+      {showCart && cartContent}
+      <header>
+        <h1>T-Shirt Store</h1>
+        <button className="cart-button" onClick={showCartHandler}>
+          Cart ({cartItems.length})
+        </button>
+      </header>
+      <main>
+        <div className="tshirt-container">
+          {tshirts.map((tshirt) => (
+            <div key={tshirt.id} className="tshirt">
+              <h3>{tshirt.name}</h3>
+              <p>{tshirt.description}</p>
+              <p>Price: ${tshirt.price.toFixed(2)}</p>
+              <p>Quantity: {tshirt.quantity}</p>
+              <div className="size-buttons">
+                <button className="size-button">Small</button>
+                <button className="size-button">Medium</button>
+                <button className="size-button">Large</button>
+              </div>
+              <button
+                className="add-button"
+                onClick={() => addToCartHandler(tshirt)}
+                disabled={tshirt.quantity === 0}
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
         </div>
-        <div className="food-item">
-          <img src={pizzaImage} alt="Pizza" />
-          <p>Pizza</p>
-          <p>Price: $12</p>
-          <button onClick={() => addToCart('Pizza')}>Add to Cart</button>
+      </main>
+      <footer>
+        <div className="model-box">
+          <h3>Model Box</h3>
+          <button className="buy-button">Buy Small</button>
+          <button className="buy-button">Buy Medium</button>
+          <button className="buy-button">Buy Large</button>
+          <p>Total Price: ${(cartItems.length * tshirts[0].price).toFixed(2)}</p>
+          <button className="order-button">Order</button>
+          <button className="cancel-button">Cancel</button>
         </div>
-        <div className="food-item">
-          <img src={saladImage} alt="Salad" />
-          <p>Salad</p>
-          <p>Price: $8</p>
-          <button onClick={() => addToCart('Salad')}>Add to Cart</button>
-        </div>
-      </div>
-      <div className="cart">
-        <h2>Cart</h2>
-        {cartItems.length === 0 ? (
-          <p>Cart is empty</p>
-        ) : (
-          <div>
-            <ul>
-              {cartItems.map((item, index) => (
-                <li key={index}>
-                  {item.name} - Quantity: {item.quantity} - Price: ${item.price * item.quantity}
-                  <button onClick={() => incrementQuantity(item.name)}>+</button>
-                  <button onClick={() => decrementQuantity(item.name)}>-</button>
-                  <button onClick={() => removeFromCart(item.name)}>Remove</button>
-                </li>
-              ))}
-            </ul>
-            <p>Total Price: ${calculateTotalPrice()}</p>
-          </div>
-        )}
-      </div>
+      </footer>
     </div>
   );
 };
 
-export default FoodApp;
+export default App;
